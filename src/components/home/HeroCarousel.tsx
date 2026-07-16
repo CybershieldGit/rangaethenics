435e9fd06524c6ffd0e3745dfb5677b4b78732bf
azad <team.cybershield@gmail.com>
2026-07-16 17:30:54 +0530

    added fixes

--- src/components/home/HeroCarousel.tsx
+++ src/components/home/HeroCarousel.tsx
@@ -80,7 +80,7 @@ export function HeroCarousel() {
               <Link to="/about">
                 <Button className="!px-4 !py-2 text-xs md:!px-8 md:!py-3 md:text-sm cursor-pointer">Our Story</Button>
               </Link>
-              <Link to="/">
+              <Link to="/products?category=all">
                 <Button variant="outline" className="!px-4 !py-2 text-xs md:!px-8 md:!py-3 md:text-sm cursor-pointer">
                   Explore Collections
                 </Button>
